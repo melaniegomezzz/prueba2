543b97a439ebc87a14a4c0af11b0cbaac6afb86f
jtg1393 <melaniegomezzpz@gmail.com>
2025-11-12 09:39:42 -0600

    agregue el App.tsx

--- App.tsx
+++ App.tsx
@@ -1,45 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+import React, {useState} from 'react';
+import {Text, TextInput, View} from 'react-native';
+const PizzaTranslator = () => {
+  const [text, setText] = useState('');
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
+    <View style={{flex: 1, justifyContent: 'center'}}>
+      <Text style={{fontSize: 25}}>Esta aplicación es un traductor, cada palabra se traduce como una rebanada de pizza</Text>
+      <TextInput
+        placeholder="escribe para transcribir!"
+        onChangeText={newText => setText(newText)}
+        defaultValue={text}
+        style={{
+          height: 80,
+          padding: 10,
+          marginHorizontal: 8,
+          borderWidth: 1,
+          fontSize: 20,
+        }}
       />
+      <Text style={{padding: 50, fontSize: 40}}>
+        {text
+          .split(' ')
+          .map(word => word && '🍕')
+          .join(' ')}
+      </Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
+export default PizzaTranslator;
